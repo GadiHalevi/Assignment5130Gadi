2e257b2e9c175fe06e0030bd225d00eb01a447e3
Gadi Halevi <gadihalevi@gmail.com>
2020-02-21 12:38:01 +0200

    /

--- Assignment5130Gadi/Assignment5130Gadi/views.py
+++ Assignment5130Gadi/Assignment5130Gadi/views.py
@@ -5,11 +5,41 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from Assignment5130Gadi import app
+from Assignment5130Gadi.Models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+
+
+from Assignment5130Gadi.Models.QueryFormStructure import LoginFormStructure 
+from Assignment5130Gadi.Models.QueryFormStructure import UserRegistrationFormStructure 
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    """Renders the home page."""
     return render_template(
         'home.html',
         title='Home Page',
@@ -18,7 +48,6 @@ def home():
 
 @app.route('/contact')
 def contact():
-    """Renders the contact page."""
     return render_template(
         'contact.html',
         title='Contact',
@@ -28,7 +57,6 @@ def contact():
 
 @app.route('/about')
 def about():
-    """Renders the about page."""
     return render_template(
         'about.html',
         title='About',
@@ -38,18 +66,48 @@ def about():
 
 @app.route('/datamodel')
 def datamodel():
-    """Renders the about page."""
     return render_template(
         'datamodel.html',
         title='Data Model',
         year=datetime.now().year,
     )
 
-@app.route('/register')
-def register():
-    """Renders the about page."""
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
     return render_template(
-        'register.html',
-        title='Register',
+        'register.html', 
+        form=form, 
+        title='Register New User',
         year=datetime.now().year,
-    )
+        repository_name='Pandas',
+        )
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+        else:
+            flash('Error in - Username and/or password')
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
