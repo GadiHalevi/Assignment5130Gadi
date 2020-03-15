8a3957ec42c6bee881e79f33c880f6fa342126c3
Gadi Halevi <gadihalevi@gmail.com>
2020-03-15 08:53:57 +0200

    .

--- Assignment5130Gadi/Assignment5130Gadi/views.py
+++ Assignment5130Gadi/Assignment5130Gadi/views.py
@@ -114,7 +114,7 @@ def Login():
     if (request.method == 'POST' and form.validate()):
         if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
             flash('Login approved!')
-            return redirect('/DataQuery')
+            return redirect("http://gadihalevi.pythonanywhere.com/DataQuery")
 
         else:
             flash('Error in - Username and/or password')
